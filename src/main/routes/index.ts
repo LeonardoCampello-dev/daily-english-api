7dd61948d71ce7849e31742848692c5f5fdae753
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-01-13 11:20:52 -0300

    feat: add phrase crud

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { words } from './words';
+import { phrases } from './phrases';
 
 const router = Router();
 
 router.use('/words', words);
+router.use('/phrases', phrases);
 
 export { router };
---
+++ src/main/routes/phrases.ts
@@ -0,0 +1,72 @@
+import { Router, Request, Response } from 'express';
+
+import { Crud, ErrorHandler } from '../../application/helpers';
+import { Phrase } from '../../domain/entities';
+import { faunaClient } from '../config/fauna-client';
+
+const router = Router();
+const crudService = new Crud<Phrase>('sentences', faunaClient);
+const errorHandler = new ErrorHandler();
+
+type Body = Omit<Phrase, 'id'>;
+
+router.get('/:id', async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await crudService.get<Phrase>(id);
+
+    response.json(result.data);
+  } catch (error) {
+    const formattedError = errorHandler.handle(error.requestResult.statusCode);
+
+    response.status(formattedError.status).json(formattedError);
+  }
+});
+
+router.post('/', async (request: Request, response: Response) => {
+  try {
+    const { body } = request;
+
+    const result = await crudService.create<Body, Phrase>(body);
+
+    response.json(result.data);
+  } catch (error) {
+    const formattedError = errorHandler.handle(error.requestResult.statusCode);
+
+    response.status(formattedError.status).json(formattedError);
+  }
+});
+
+router.put('/:id', async (request: Request, response: Response) => {
+  try {
+    const {
+      body,
+      params: { id }
+    } = request;
+
+    const result = await crudService.update<Body, Phrase>(id, body);
+
+    response.json(result.data);
+  } catch (error) {
+    const formattedError = errorHandler.handle(error.requestResult.statusCode);
+
+    response.status(formattedError.status).json(formattedError);
+  }
+});
+
+router.delete('/:id', async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await crudService.delete<Phrase>(id);
+
+    response.json(result.data);
+  } catch (error) {
+    const formattedError = errorHandler.handle(error.requestResult.statusCode);
+
+    response.status(formattedError.status).json(formattedError);
+  }
+});
+
+export { router as phrases };
